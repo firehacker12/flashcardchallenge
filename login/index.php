797f00e74b0d442f9c97112d4faf37a4cdd5bbd2
firehacker12 <halestorm512@gmail.com>
2020-09-23 08:58:54 -0400

    Initial commit

---
+++ login/index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<html>
+  <head>
+    <title>Quizzard</title>
+    <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
+    <script src="https://kit.fontawesome.com/4a43f383fb.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="../home">
+          <img src="../img/logo.png" width="128px" />
+        </a>
+      </div>
+    </nav>
+  </body>
+</html>
