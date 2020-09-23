d7e2dd3bc6179b57a618ee9ba6a5cf9e4e0e4741
firehacker12 <halestorm512@gmail.com>
2020-09-23 14:29:41 -0400

    ed

--- login/index.php
+++ login/index.php
@@ -7,12 +7,13 @@
     <title>Quizzard</title>
     <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
     <script src="https://kit.fontawesome.com/4a43f383fb.js" crossorigin="anonymous"></script>
+    <link href="style.css" type="text/css" rel="stylesheet" />
   </head>
   <body>
     <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <a class="navbar-item" href="../home">
-          <img src="../img/logo.png" width="128px" />
+          <img src="../img/logo.png" width="64px" />
         </a>
       </div>
     </nav>
