7ad8fbb27c83a76bf3ea3c5192ae2b4ae35c055b
TheLumine0 <54721817+TheLumine0@users.noreply.github.com>
2020-09-23 16:17:07 -0400

    e

--- client/game/socket.js
+++ client/game/socket.js
@@ -1,1 +1,11 @@
 var socket = io(":2000");
+
+function roomMake(){
+  socket.emit('createRoom',{'a'},'yes','teacher',{'settings'});
+}
+
+function roomJoin(){
+  let roomTmp = document.getElementById('enterCode').value;
+  let nameTmp = document.getElementById('enterName').value;
+  socket.emit('joinRoom',code,name);
+}
