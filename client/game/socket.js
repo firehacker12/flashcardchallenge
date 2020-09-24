cbaa68fe0cb961293c776e27ea51a6b170bdeab1
TheLumine0 <54721817+TheLumine0@users.noreply.github.com>
2020-09-24 16:38:36 -0400

    p

--- client/game/socket.js
+++ client/game/socket.js
@@ -20,6 +20,7 @@ socket.on('joinLobby',(room,id_) => {
   document.getElementById('joinMake').setAttribute("style","display:none");
   document.getElementById('lobbyScreen').setAttribute("style","");
   id = id_;
+  document.getElementById('lobbyCode').innerHTML = room.id;
   document.getElementById('studentLobbyList').innerHTML = "";
   for(var i=0; i<room.students.length; i++){
     document.getElementById('studentLobbyList').innerHTML += "<p1>"+room.students[i].name+"</p1><br>";
@@ -27,6 +28,7 @@ socket.on('joinLobby',(room,id_) => {
 });
 
 socket.on('updateLobby',(room) => {
+  document.getElementById('lobbyCode').innerHTML = room.id;
   document.getElementById('studentLobbyList').innerHTML = "";
   for(var i=0; i<room.students.length; i++){
     document.getElementById('studentLobbyList').innerHTML += "<p1>"+room.students[i].name+"</p1><br>";
