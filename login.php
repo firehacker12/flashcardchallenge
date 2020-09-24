53e181ed201a3cc29038e97599ac4dc120304971
firehacker12 <halestorm512@gmail.com>
2020-09-24 16:35:07 -0400

    e

--- login.php
+++ login.php
@@ -4,16 +4,21 @@
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  $password = password_hash($password, PASSWORD_DEFAULT);
-
-  $query = "SELECT * FROM login WHERE Username='$username'";
+  $query = "SELECT * FROM login";
 
   $response = @mysqli_query($conn, $query);
+
+  echo $username;
 
   if ($response) {
     while ($row = mysqli_fetch_array($response)) {
       if ($row['Username'] == $username || $row['Email'] == $username) {
-        echo "Found username";
+        if (password_verify($password, $row['Password'])) {
+          echo "Correct password";
+        }
+        else {
+          echo "no, that's not right";
+        }
       }
     }
   }
